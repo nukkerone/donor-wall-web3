b4b5cc6ba0f352ba4742ef07a09c9247c8068fe0
Gabriel Medina <gabi89.m@gmail.com>
2022-10-23 20:41:02 -0300

    Quicknode hardhat config, added dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").config();
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -11,4 +13,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  }
 };
